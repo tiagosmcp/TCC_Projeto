636b9f6660e22a398dcc8fc07cfab13376391d28
tiagosmcp <103256731+tiagosmcp@users.noreply.github.com>
2025-09-27 14:09:54 +0000

    front login

--- src/pages/LoginTela/Login.jsx
+++ src/pages/LoginTela/Login.jsx
@@ -7,29 +7,35 @@ import { Link } from "react-router-dom";
 
 const Login = () =>{
     return(
-        <div className='cardLogin'>
-            <form action=''>
-               {/* <div className="logoLogin">
-                    <img src={ipbLogo}/>
-                </div>*/}
-            </form>
-            <div className='input'>
-                <input type='text' placeholder='Usuário' required/>
-                <FaUser className='icon'/>
-            </div>
-            <div className='input'>
-                <input type='password' placeholder='Senha' required/>
-                <FaLock className='icon'/>
-            </div>
-            <div className='lembrarSenha'>
-                <label><input type='checkbox'/>Guardar Senha</label>
+            <div className="loginPage">
+                <div className='cardLogin'>
+                    <div className="logoLogin">
+                    <img src={ipbLogo} alt="Logo IPB"/>
+                    </div>
+                    <form>
+                    <div className='input'>
+                        <input type='text' placeholder='Usuário' required/>
+                        <FaUser className='icon'/>
+                    </div>
+                    <div className='input'>
+                        <input type='password' placeholder='Senha' required/>
+                        <FaLock className='icon'/>
+                    </div>
+                    <div className='lembrarSenha'>
+                        <label><input type='checkbox'/>Guardar Senha</label>
+                    </div>
+
+                    <div className="loginButtons">
+                        <Link to="/">
+                            <button className='botoes'> <FaArrowLeft />  Voltar </button>
+                        </Link>
+                            <button type='submit' className='botoes'>Entrar</button>
+                    </div>
+
+                    </form>
+                </div>
             </div>
 
-            <Link to="/">
-                <button className='botoes'> <FaArrowLeft />  Voltar </button>
-            </Link>
-            <button type='submmit' className='botoes'>Entrar</button>
-        </div>
     );    
 };
 
