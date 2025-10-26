ae8357bd798f0566ca5e0201eeb87292e67981b6
Tiago Soares <tiago080304@gmail.com>
2025-10-26 13:52:40 -0300

    subindo api no render

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,22 @@
-import express from "express"
-import programacaoRoutes from "./routes/programacao.js" 
-import authRoutes from "./routes/auth.js" 
-import cors from "cors"
+import express from "express";
+import programacaoRoutes from "./routes/programacao.js";
+import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 // Configuração de CORS para permitir o front na vercel
-app.use(cors({
-    origin: 'https://ipbalvorada.vercel.app/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true, // Necessário se você usar cookies/sessões (boas práticas)
-}));
+app.use(
+    cors({
+        origin: "https://ipbalvorada.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true, // Necessário se você usar cookies/sessões (boas práticas)
+    })
+);
 
-app.use("/programacao", programacaoRoutes) 
-app.use("/auth", authRoutes)
+app.use("/programacao", programacaoRoutes);
+app.use("/auth", authRoutes);
 
-app.listen(8800, () => console.log("Servidor rodando na porta 8800"))
+app.listen(8800, () => console.log("Servidor rodando na porta 8800"));
