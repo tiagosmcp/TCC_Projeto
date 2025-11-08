4415c852aa27d1a49dde7036f5994a9cbb589536
Tiago Soares <tiago080304@gmail.com>
2025-11-08 17:55:23 -0300

    ajuste CORS para vercel

--- src/api/index.js
+++ src/api/index.js
@@ -10,12 +10,13 @@ app.use(express.json());
 // Configuração de CORS para permitir o front na vercel
 app.use(
     cors({
-        origin: "https://ipbalvorada.vercel.app",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true, // Necessário se você usar cookies/sessões (boas práticas)
+        origin: ["https://ipbalvorada.vercel.app", "http://localhost:3000"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+        credentials: true,
     })
 );
-
+app.options("*", cors());
 app.use("/programacao", programacaoRoutes);
 app.use("/auth", authRoutes);
 
