00527228f5f6a1586926292607f88b0feb35b1b7
Tiago Soares <tiago080304@gmail.com>
2025-11-19 12:44:39 -0300

    Corrigindo conexao banco

--- src/api/db.js
+++ src/api/db.js
@@ -1,22 +1,13 @@
 import mysql from "mysql2";
 
-export const db = mysql.createConnection({
-  host: "mainline.proxy.rlwy.net", 
-  port: 28212,                  
-  user: "root",                    
-  password: "ncmnVtQIjwvMZVaKitUrBlxcNUtsgykf",    
-  database: "ipb_alvorada",           
+export const db = mysql.createPool({
+  host: "mainline.proxy.rlwy.net",
+  port: 28212,
+  user: "root",
+  password: "ncmnVtQIjwvMZVaKitUrBlxcNUtsgykf",
+  database: "ipb_alvorada",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-
-db.connect((err) => {
-    if (err) {
-        console.error("❌ ERRO AO CONECTAR AO MYSQL: ", err.stack);
-        console.log(
-            "Certifique-se de que o servidor MySQL está rodando e que as credenciais em db.js estão corretas."
-        );
-        // Força a saída do processo se a conexão falhar
-        process.exit(1);
-    }
-    console.log("✅ Conexão MySQL estabelecida com ID: " + db.threadId);
-});
